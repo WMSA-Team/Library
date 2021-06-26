5b8376cf46600faf0d380a9eafc8df4cda2a710b
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2021-06-26 16:57:43 +0300

    adding popup box

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-'use strict'
+'use strict';
+function openFormButton(event){
+  event.preventDefault();
+  document.getElementById('myForm').style.display = 'block';
+
+}
+function closeButton(event){
+  event.preventDefault();
+  document.getElementById('myForm').style.display = 'none';
+}
