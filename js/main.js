da63bf9b928572521e9c0274cc4854860bc77c00
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-06-27 10:27:19 +0300

    added ratingconfig

--- js/main.js
+++ js/main.js
@@ -1,55 +1,7 @@
 
 'use strict'
 
-// window.addEventListener("DOMContentLoaded", setupForm);
-
 let user;
-// function setupForm(){
-// 	const form = document.getElementById("login-form");
-// 	form.addEventListener("submit", handleSubmit);
-// }//setupForm
-
-// function handleSubmit(event){
-// 	event.preventDefault();
-// 	const {
-// 		username,
-// 		password,
-// 		login_type,
-// 		result
-// 	} = event.target;
-// 	const processUser = login_type.value == "register" ? registerUser : loginUser;
-// 	const response    = processUser(username.value, password.value);
-// 	result.innerHTML  = response;
-// }//handleSubmit
-
-// function registerUser(username, password){
-// 	window.localStorage.setItem("username", username);
-// 	window.localStorage.setItem("password", password);
-// 	return `New user ${username} now registered!`;
-// }//registerUser
-
-// function loginUser(username, password){
-// 	const registeredUser     = window.localStorage.getItem("username");
-// 	const registeredPassword = window.localStorage.getItem("password");
-// 	const validUser     = username == registeredUser;
-//     user=registeredUser
-// 	const validPassword = password == registeredPassword;
-
-// 	if(validUser && validPassword){
-//          document.getElementById("login-form").innerHTML='';
-//          appendText();
-
-// 	}
-//         else if(!validUser)     return `Username ${username} has not been registered. `;
-// 	else if(!validPassword) return `Incorrect password for username ${username}`;
-// }
-// function appendText() {
-//     let text = document.createElement('p');
-//     const form = document.getElementById("login-form")
-//     text.innerText= `welcome ${user} .`;
-//     form.appendChild(text);
-//   };
-
 
 function openFormButton(event){
   event.preventDefault();
@@ -60,27 +12,17 @@ function closeButton(event){
   document.getElementById('myForm').style.display = 'none';
 }
 
-/// mamoun
-///this function and event handler to make the content change with each category of the check boxes
 let checkboxes = document.querySelectorAll("input[type=checkbox]");
 let cardContainer = document.getElementById('cardContainer');
 let rating = document.getElementById('rating')
-// rating.removeAttribute('id')
-// console.log(rating);
 function render (ar) {
     cardContainer.innerHTML = ''
     let count = 100000000;
-    // for (let i=0; i<ar.length; i++) {
-        //this line will check all the book we have checked and render them on the cardContainer section
-        // for (let j=0; j<Books.arr.cat.length; j++) {
-            //creat card div element
             for (let i = 0; i<ar.length; i++){
             let card = document.createElement('div');
             card.className = 'card'
-            // create side div element
             let side = document.createElement('div');
             side.className = 'side'
-            // create img element
             let imag = document.createElement('img');
             imag.src = ar[i].imag;
             let section1 = document.createElement('section');
@@ -94,7 +36,6 @@ function render (ar) {
             action.className = 'action'
             let buy = document.createElement('button')
             buy.innerText = 'Buy'
-            // rating.setAttribute('id', 'mamoon')
             side.appendChild(imag);
             card.appendChild(side);
             sideBack.appendChild(section1);
@@ -102,7 +43,6 @@ function render (ar) {
             section1.appendChild(p);
             card.appendChild(sideBack);
             action.appendChild(buy);
-            // action.appendChild(rating);
             sideBack.appendChild(action)
             cardContainer.appendChild(card)
             let star = document.createElement('div')
@@ -122,14 +62,14 @@ function render (ar) {
                 label.className = `star star-${j}`
                 form.appendChild(input)
                 label.setAttribute('for',`star${j+count}`) ;
-                // label.innerText = j
                 form.appendChild(label);
+                if (ar[i].rating === j) {
+                    input.checked = true;
+                }
             }
             star.appendChild(form)
             action.appendChild(star);
             }
-        // }
-    // }
  }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('change',change))
@@ -146,12 +86,8 @@ function change (e) {
                 bookCat.splice(index,1);
         }
     }
-    // console.log(bookCat);
-    // render (bookCat);
 }
 
-
-//////End of mamoon Code
 
 function Book (name,description,imag) {
     this.name = name;
@@ -165,16 +101,7 @@ let Hyper = new Book('HYPER SPACE','aaaaa','https://www.adobe.com/express/discov
 let WHAT = new Book ('WHAT IF', 'any', 'https://dboyle93.files.wordpress.com/2014/05/the-road-cover-basic4.png')
 let DARK = new Book ('DARK MATTER','walaa','../img/SCIENCEIMG/DARK MATTER AND THE DINOSTORS.jpg');
 let ABRIEF = new Book ('ABRIEF HISTORY OF TIME', 'shahed', '../img/SCIENCEIMG/ABRIEF HISTORY OF TIME.jpg')
+DARK.rating = 3;
+Hyper.rating = 5;
 render(Book.arr);
-
-
-
- let bk =['HYPER SPACE','WHAT IF','DARK MATTER AND THE DINOSTORS','ABRIEF HISTORY OF TIME','THE WRIGHT BROTHERS','MAKING CONTACT',
-'THE DEMON-HAUNTED WORLD','IN LINITE POWERS','PHILOSOPHY OF SCIENCE','A SHORT HISTORY','100 THINGS CODING','A CRACK IN CREATION',
-'A STROPHYSICS','THE DREAMT LAND','THE MAD SCIENCE BOOK','THE BOOK OF WHY','THE SELFISH GENE','CARL SAGAN COSMOS','DATA SCIENCE',
-'ALAB OF ONES OWN','WORLD HISTORY','ANCIENT WORLD','THE BOOK IN BRITAIN','THE SHADOW KING','FALL OF GIANTS','GUNS,GERMS & STEEL','AN ORPHANS WAR','THE AGE AUGUSTUS','HAMILTION','THE FOUNTAINS OF SILENCE','BLOOD SUGAR','ADOLF HITLER','LOST ISLAMIC HISTORY',
-'THE POWER OF BABEL','THE TOWER OF BABEL','MOSQUES','HOMAGE TO AL-ANDALUS','ANCIENT EGYPT',
-'THE BOOK OF THE PHARAOHS','THE GOLDEN AGE',
- ];
-
 
