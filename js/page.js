9dbce2003317a563a132916e3da71384f546b28b
walaamohammad <layan_abadi86@yahoo.com>
2021-06-29 10:31:07 +0300

    addcssdesign

--- js/page.js
+++ js/page.js
@@ -119,7 +119,7 @@ function typeWriter(id, ar) {
             let h1 = document.createElement('h1')
             h1.innerText = ar[random].name;
             let p = document.createElement('p')
-            p.innerText = `${ar[random].description.slice(0,99)}......`;
+            p.innerText = `${ar[random].description.slice(0,99)}..`;
             let sideBack = document.createElement('div')
             sideBack.className = 'side back'
             let action = document.createElement('section')
